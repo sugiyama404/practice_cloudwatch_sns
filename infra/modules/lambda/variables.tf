918071814f524634638047fc6a9a1ac74cae20c1
sugiyama404 <mmingriha@gmail.com>
2024-07-07 11:40:57 +0900

    first commit

---
+++ infra/modules/lambda/variables.tf
@@ -0,0 +1,4 @@
+variable "app_name" {}
+variable "lambda_iam_role" {}
+variable "lambda_function_name" {}
+variable "sns_topic_arn" {}
---
+++ webserver/app/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+// 各ルートの設定
+const routes = [
+    { path: '/', action: 'index', message: 'ホーム画面です\n' },
+    { path: '/about', action: 'about', message: 'これはAboutページです\n' },
+    { path: '/faq', action: 'faq', message: 'これはFAQページです\n' },
+    { path: '/portfolio', action: 'portfolio', message: 'これはPortfolioページです\n' },
+    { path: '/error', action: 'error', message: 'これはerrorページです\n' },
+];
+
+routes.forEach(route => {
+    app.get(route.path, (req, res) => {
+        try {
+            if (route.action === 'error') {
+                throw new Error('An error page was accessed.');
+            } else {
+                res.send(route.message);
+            }
+        } catch (err) {
+            console.error('Fatal error:', err.message);
+            res.status(500).send('致命的なエラーが発生しました\n');
+        }
+        res.send(route.message);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
