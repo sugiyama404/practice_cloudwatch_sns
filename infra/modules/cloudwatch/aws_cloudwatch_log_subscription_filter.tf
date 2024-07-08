0a9fe7d0808664d3b34c646bfc84d5e424e8703d
sugiyama404 <mmingriha@gmail.com>
2024-07-08 11:18:21 +0900

    check complete email

--- infra/modules/cloudwatch/aws_cloudwatch_log_subscription_filter.tf
+++ infra/modules/cloudwatch/aws_cloudwatch_log_subscription_filter.tf
@@ -1,9 +1,6 @@
 resource "aws_cloudwatch_log_subscription_filter" "lambda_trigger" {
-  name           = "ErrorLogsToLambda"
-  log_group_name = var.webserver_log_group_name
-  # filter_pattern  = "{ $.level = \"error\" || $.level = \"fatal\" }"
-  filter_pattern  = ""
+  name            = "ErrorLogsToLambda"
+  log_group_name  = var.webserver_log_group_name
+  filter_pattern  = "error"
   destination_arn = var.lambda_function_arn
-
-  # depends_on = [module.lambda.aws_lambda_permission.allow_cloudwatch]
 }
